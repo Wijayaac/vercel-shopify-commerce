a914eebeae2b8b33e05892f601fd6e137967298e
Wijayaac <wijayaac25@gmail.com>
2024-03-28 11:46:14 +0700

    Update : progress bar checkpoint

--- components/cart/tier-discount.tsx
+++ components/cart/tier-discount.tsx
@@ -24,7 +24,7 @@ export default function TierDiscount({ tieredDiscount }: { tieredDiscount: any }
 
   return (
     <>
-      <div className="mt-2 flex flex-col items-center justify-between gap-2 border-b border-neutral-200 pb-4 md:mt-4 dark:border-neutral-700">
+      <div className="mt-2  flex flex-col items-center justify-between gap-2 border-b border-neutral-200 pb-4 md:mt-4 dark:border-neutral-700">
         <div className="flex gap-2">
           {spentToNextDiscount > 0 && (
             <>
@@ -38,20 +38,39 @@ export default function TierDiscount({ tieredDiscount }: { tieredDiscount: any }
           )}
           {nextDiscount && <span>to get {nextDiscount * 100}%</span>}
         </div>
-        <div className="relative h-2.5 w-full  overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700">
-          <div
-            className="h-2.5 rounded-full bg-blue-600"
-            style={{ width: `${(subTotal / minSpent) * 100}%` }}
-          />
-          {discountGroups.map((group: DiscountGroup, i: number) => (
-            <div
-              key={i}
-              className={`absolute top-0 h-2.5 rounded-full bg-blue-${
-                i === 0 ? 400 : 200
-              } dark:bg-blue-${i === 0 ? 400 : 700}`}
-              style={{ width: `${(group.discount.minimumSpent / minSpent) * 100}%` }}
-            />
-          ))}
+        <div className="w-full px-10">
+          <div className="relative h-10 w-full">
+            <div className="h-2.5 w-full  overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700">
+              <div
+                className="h-2.5 rounded-full bg-blue-600"
+                style={{ width: `${(subTotal / minSpent) * 100}%` }}
+              />
+              {discountGroups.map((group: DiscountGroup, i: number) => (
+                <div
+                  key={i}
+                  className={`text-rigth absolute top-0 flex h-2.5 justify-end rounded-full bg-blue-${
+                    i === 0 ? 400 : 200
+                  } dark:bg-blue-${i === 0 ? 400 : 700}`}
+                  style={{ width: `${(group.discount.minimumSpent / minSpent) * 100}%` }}
+                >
+                  <span className="h-[10px] w-[10px] rounded-full bg-white"></span>
+                </div>
+              ))}
+            </div>
+            {discountGroups.map((group: DiscountGroup, i: number) => (
+              <div
+                key={i}
+                className={`text-rigth absolute bottom-2 flex h-2.5 justify-end rounded-full bg-blue-${
+                  i === 0 ? 400 : 200
+                } dark:bg-blue-${i === 0 ? 400 : 700}`}
+                style={{ width: `${(group.discount.minimumSpent / minSpent) * 100}%` }}
+              >
+                <span className="inline-flex translate-x-4 transform text-center">
+                  {group.discount.amount * 100}%
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
         <p className="text-base text-black dark:text-white">
           {spentToNextDiscount === 0 ? (
